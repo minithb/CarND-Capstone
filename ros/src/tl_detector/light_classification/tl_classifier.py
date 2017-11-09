7171eb9443c430a6294774a2ee91034a66f50231
Minith Bharat Jain <minithsatawat@hotmail.com>
2017-11-09 19:54:33 +0530

    TL Classifier using SVM

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -8,22 +8,35 @@ import math
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
+
+
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
         #self.model_path = model_path
         #self.labels = [TrafficLight.NaN, TrafficLight.R, TrafficLight.Y, TrafficLight.G, TrafficLight.NaN]
         #PATH_TO_CKPT = self.model_path + '/checkpoints/frozen_inference_graph.pb'
-        self.graph = tf.Graph()
-        with self.graph.as_default():
-            gpu_options = tf.GPUOptions(allow_growth=True)
-            config = tf.ConfigProto(log_device_placement=False, gpu_options=gpu_options)
-            self.sess = tf.Session(config=config)
+        # Load LinearSVC Model using load function
+	    self.svc = joblib.load('LinearSVC.pkl')
+	    
+	    # Load Scaler using load function
+	    self.X_scaler = joblib.load('LinearScaler.pkl')
+	    self.nbins=32
+	    self.bins_range=(0, 256)
+
+        #self.graph = tf.Graph()
+        #with self.graph.as_default():
+        #    gpu_options = tf.GPUOptions(allow_growth=True)
+        #    config = tf.ConfigProto(log_device_placement=False, gpu_options=gpu_options)
+        #    self.sess = tf.Session(config=config)
                 #sess.run(tf.initialize_all_variables())
-            server = tf.train.import_meta_graph('lenet.meta')
-            server.restore(sess, tf.train.latest_checkpoint('./'))
-            self.input_image = tf.get_default_graph().get_tensor_by_name("input_image:0")
-            self.model_output = tf.get_default_graph().get_tensor_by_name("model_output:0")
+        #    server = tf.train.import_meta_graph('lenet.meta')
+        #    server.restore(sess, tf.train.latest_checkpoint('./'))
+        #    self.input_image = tf.get_default_graph().get_tensor_by_name("input_image:0")
+        #    self.model_output = tf.get_default_graph().get_tensor_by_name("model_output:0")
         #pass
 
     def get_classification(self, image):
@@ -38,9 +51,26 @@ class TLClassifier(object):
         """
         #TODO implement light color prediction
         #return TrafficLight.UNKNOWN
-        with self.graph.as_default():
-            classes = self.sess.run(self.model_output, {
-                self.input_image: [image]
-            })[0]
-            return np.argmax(classes)
+		img = image[50:150,190:490]
+    
+	    channel1_hist = np.histogram(img[:,:,0], bins=self.nbins, range=self.bins_range)
+	    channel2_hist = np.histogram(img[:,:,1], bins=self.nbins, range=self.bins_range)
+	    channel3_hist = np.histogram(img[:,:,2], bins=self.nbins, range=self.bins_range)
+	    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+	    # Scale features and make a prediction
+	    test_features = self.X_scaler.transform(hist_features)
+	    test_prediction = self.svc.predict(test_features)
+	    if (test_prediction[0] == 0):
+	        return TrafficLight.RED
+	    elif (test_prediction[0] == 1):
+	        return TrafficLight.GREEN
+	    elif (test_prediction[0] == 2):
+	        return TrafficLight.YELLOW
+	    else:
+	        return TrafficLight.UNKNOWN
 
+        #with self.graph.as_default():
+        #    classes = self.sess.run(self.model_output, {
+        #        self.input_image: [image]
+        #    })[0]
+        #    return np.argmax(classes)
