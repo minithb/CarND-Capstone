68764f143a049edd05957e6a55d112e0b4b146fa
Alex Darwish <alex.darwish@weatheranalytics.com>
2017-11-09 14:09:15 -0500

    fixed identation issues

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,3 +1,4 @@
+ 
 from styx_msgs.msg import TrafficLight
 import pickle 
 from sklearn.model_selection import train_test_split
@@ -38,7 +39,6 @@ class TLClassifier(object):
         #    self.input_image = tf.get_default_graph().get_tensor_by_name("input_image:0")
         #    self.model_output = tf.get_default_graph().get_tensor_by_name("model_output:0")
         #pass
-
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
         Args:
@@ -48,27 +48,29 @@ class TLClassifier(object):
         """
         #TODO implement light color prediction
         #return TrafficLight.UNKNOWN
-		img = image[50:150,190:490]
+        img = image[50:150,190:490]
     
-	    channel1_hist = np.histogram(img[:,:,0], bins=self.nbins, range=self.bins_range)
-	    channel2_hist = np.histogram(img[:,:,1], bins=self.nbins, range=self.bins_range)
-	    channel3_hist = np.histogram(img[:,:,2], bins=self.nbins, range=self.bins_range)
-	    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+        channel1_hist = np.histogram(img[:,:,0], bins=self.nbins, range=self.bins_range)
+        channel2_hist = np.histogram(img[:,:,1], bins=self.nbins, range=self.bins_range)
+        channel3_hist = np.histogram(img[:,:,2], bins=self.nbins, range=self.bins_range)
+        hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
 	    # Scale features and make a prediction
-	    test_features = self.X_scaler.transform(hist_features)
-	    test_prediction = self.svc.predict(test_features)
-	    if (test_prediction[0] == 0):
-	        return TrafficLight.RED
-	    elif (test_prediction[0] == 1):
-	        return TrafficLight.GREEN
-	    elif (test_prediction[0] == 2):
-	        return TrafficLight.YELLOW
-	    else:
-	        return TrafficLight.UNKNOWN
+        test_features = self.X_scaler.transform(hist_features)
+        test_prediction = self.svc.predict(test_features)
+        if (test_prediction[0] == 0):
+            return TrafficLight.RED
+        elif (test_prediction[0] == 1):
+            return TrafficLight.GREEN
+        elif (test_prediction[0] == 2):
+            return TrafficLight.YELLOW
+        else:
+            return TrafficLight.UNKNOWN
 
         #with self.graph.as_default():
         #    classes = self.sess.run(self.model_output, {
         #        self.input_image: [image]
         #    })[0]
+        #    return np.argmax(classes)
  
- 
+
+
